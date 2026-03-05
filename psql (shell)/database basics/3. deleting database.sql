154b2bbbc66e9ef8485ff8bb074e332e3a4f9570
Muhammad Umar <muhammad.umar.218980@gmail.com>
2026-03-06 01:47:50 +0500

    Added IF EXISTS command

--- psql (shell)/database basics/3. deleting database.sql
+++ psql (shell)/database basics/3. deleting database.sql
@@ -5,3 +5,9 @@ DROP DATABASE my_database;
 -- Example: 
 -- \c postgres
 -- Then you can run the DROP DATABASE command.
+
+
+DROP DATABASE IF EXISTS my_database;
+-- This command will only delete the database if it exists, preventing errors if the database is not found.
+
+-- Note : if youre connected to the database you want to delete, you will need to connect to a different database (like 'postgres') before running this command.
