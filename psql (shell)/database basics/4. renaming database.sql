0cea3df3a16073e1c52eeebfacca63bd62be1947
Muhammad Umar <muhammad.umar.218980@gmail.com>
2026-03-06 01:49:50 +0500

    Added rename db command

---
+++ psql (shell)/database basics/4. renaming database.sql
@@ -0,0 +1,4 @@
+ALTER DATABASE my_database
+RENAME TO my_new_db;
+
+-- This command renames the database "my_database" to "my_new_db". Make sure to replace "my_database" and "my_new_db" with the actual names of your database.
