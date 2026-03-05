23176ddf4a37571ad8bf2494a90dbbef4bfeb0e4
Muhammad Umar <muhammad.umar.218980@gmail.com>
2026-03-06 01:40:59 +0500

    Added delete db command

---
+++ psql (shell)/database basics/3. deleting database.sql
@@ -0,0 +1,7 @@
+DROP DATABASE my_database;
+-- This permanently deletes the database named 'my_database' and all of its data. 
+
+-- Note : if youre connected to the database you want to delete, you will need to connect to a different database (like 'postgres') before running this command.
+-- Example: 
+-- \c postgres
+-- Then you can run the DROP DATABASE command.
