3bdebb5556cbb7e369b09bb3f97a89388d8a5dfa
Muhammad Umar <muhammad.umar.218980@gmail.com>
2026-03-06 01:32:00 +0500

    Added ; for semicolon

--- psql (shell)/database basics/2. creating database.sql
+++ psql (shell)/database basics/2. creating database.sql
@@ -1,3 +1,5 @@
 
 -- This SQL command creates a new database named "my_database".
 CREATE DATABASE my_database;
+
+-- also until we dont put ; at the end of the command, it will not execute. So make sure to end your SQL commands with a semicolon (;).
